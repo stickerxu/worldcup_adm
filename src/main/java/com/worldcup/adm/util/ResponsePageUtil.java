87d2f2dc8bc935674aa5224d24a0a99425607740
xujingyao <xujingyao@leqian.com>
2018-08-20 18:13:28 +0800

    0820 comp

--- src/main/java/com/worldcup/adm/util/ResponsePageUtil.java
+++ src/main/java/com/worldcup/adm/util/ResponsePageUtil.java
@@ -14,6 +14,6 @@ public class ResponsePageUtil {
     }
     public static void backAndRefresh(HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().write("<script>history.go(-1);</script>");
+        response.getWriter().write("<script>history.go(-1);location.reload(true)</script>");
     }
 }
